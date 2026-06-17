66a00be98e7ccea096260d0d9aca72605349346a
Akt1on <145835730+Akt1on@users.noreply.github.com>
2026-06-17 12:10:02 +0300

    Add Yandex Metrika tracking script to __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -122,6 +122,15 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
           geo: { "@type": "GeoCoordinates", latitude: 58.0105, longitude: 56.2502 },
         }),
       },
+       {
+        type: "text/javascript",
+        children: (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window,document,"script","https://mc.yandex.ru/metrika/tag.js","ym");
+        ym(109919915,"init",{ssr:true,clickmap:true,trackLinks:true,accurateTrackBounce:true,webvisor:true});,
+        },
     ],
   }),
   shellComponent: RootShell,
